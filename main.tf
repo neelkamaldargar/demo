d7e95289614dd8ccaf87c738e55877241948ead8
neelkamaldargar <85623866+neelkamaldargar@users.noreply.github.com>
2026-04-15 14:20:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 resource "azurerm_resource_group" "example_nkdemo_rg" {
   name     = "example-nkdemo_rg-1"
   location = "West Europe"
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
